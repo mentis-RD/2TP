e52db6ccb9cf788706793ae26ab04b700133013e
Rozum <80721089+mentis-RD@users.noreply.github.com>
2023-10-06 10:09:13 +0200

    Update index.js

--- ChangeColor/index.js
+++ ChangeColor/index.js
@@ -6,5 +6,5 @@ let hue = 0;
 button.addEventListener("click", () => {    
     background.classList.replace(background.classList[0], `color${Math.floor(Math.random() * 10)}`);
     text.style.color = `hsl(${hue}, 50%, 50%)`
-    hue += 30;
+    hue += 100;
 });
