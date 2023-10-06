8394ba0f267ec265d4ac79489db08513c45a586e
Rozum <80721089+mentis-RD@users.noreply.github.com>
2023-10-06 10:01:14 +0200

    Update index.js

--- ChangeColor/index.js
+++ ChangeColor/index.js
@@ -1,8 +1,10 @@
 const button = document.querySelector("button");
 const background = document.body;
 const text = document.querySelector("h1");
+let hue = 0;
 
 button.addEventListener("click", () => {    
     background.classList.replace(background.classList[0], `color${Math.floor(Math.random() * 10)}`);
-    text.style.color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+    text.style.color = `hsl(${hue}, 50%, 50%)`
+    hue += 10;
 });
